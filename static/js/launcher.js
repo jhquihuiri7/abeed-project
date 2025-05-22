05de853e563e3708095885de0458ec7b6eb9ed79
Milton Segura <msegura@researchcapital.com>
2025-05-22 10:42:29 -0400

    DEV: Simplify html, update javascript

--- static/js/launcher.js
+++ static/js/launcher.js
@@ -1,10 +1,26 @@
-document.querySelector(".content").addEventListener("click", (event) => {
-  const button = event.target;
-  if (button.tagName === "BUTTON" && button.dataset.filename) {
-    const fileName = button.dataset.filename;
-    send_presaved_json(fileName);
+// launcher.js
+
+init();
+
+function init() {
+  document.addEventListener("DOMContentLoaded", () => {
+    add_click_listeners();
+  });
+
+  function add_click_listeners() {
+    document.querySelector("#new-session").addEventListener("click", () => {
+      window.location.href = "/home?session_flag=new";
+    });
+
+    document.querySelector(".content").addEventListener("click", (event) => {
+      const button = event.target;
+      if (button.tagName === "BUTTON" && button.dataset.filename) {
+        const fileName = button.dataset.filename;
+        send_presaved_json(fileName);
+      }
+    });
   }
-});
+}
 
 function send_presaved_json(fileName) {
   fetch(`predefined/${fileName}`)
